ab3540516f280b60c609f02a1e9d663d42c346b4
eugene isingizwe <eugeneisingizwe95@hotmail.com>
2022-07-27 12:43:00 -0600

    added js to my code

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var topics = ['HTML', 'css', 'Git', 'javaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+
+    console.log(topics[x]);
+}
+
+/*
+
+var topic = "HTML"
+
+if (topic ==='HTML') {
+    console.log("lets study HTML!");
+}else if (topic === 'CSS') {
+    console.log("lets study CSS!");
+}else if (topic === 'Git'){
+    console.log("Lets study Git!");
+}else if (topic === 'javaScript'){
+    console.log("lets study javeScript!");
+}else{
+    console.log('please try again!');
+}
+
+
+var shapes =["triangle","quare","pentagon","circle"];
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x])
+}
+*/
